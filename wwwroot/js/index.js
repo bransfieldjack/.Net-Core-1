e43bed4f9b514350f576a08160a54e86f0eb3feb
Jack Bransfield <bransfieldjack@gmail.com>
2019-03-17 08:52:54 +1100

    Initial commit.

---
+++ wwwroot/js/index.js
@@ -0,0 +1,18 @@
+﻿var x = 0;
+var s = "";
+
+consol.log("Hello Pluralsight");
+
+
+var theForm = $("#theForm");
+theForm.hide();
+
+var button = $("#buyButton");
+button.on("click"), function () {
+    console.log("Buying Item");
+});
+
+var productInfo = $(".product-props");
+productInfo.on("click", function () {
+    )
+
